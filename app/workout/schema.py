6690f18961d53cdc686a1f2845a69337282e62d9
jeonghoonyu <moning02004@naver.com>
2023-02-18 22:57:41 +0900

    database 연동

--- app/workout/schema.py
+++ app/workout/schema.py
@@ -6,6 +6,10 @@ class Workout(BaseModel):
     name: str
     description: str
 
+    # db 데이터를 mapping 하도록 해주는 설정
+    class Config:
+        orm_mode = True
+
 
 class WorkoutCreateModel(BaseModel):
     name: str
--- app/workout/view.py
+++ app/workout/view.py
@@ -1,21 +1,20 @@
-from app.workout.mock import WORKOUT_LIST
-from app.workout.schema import Workout, WorkoutCreateModel
+from sqlalchemy.orm import Session
+
+from app.workout import crud, models
+from app.workout.schema import WorkoutCreateModel
 
 
-def _get_list():
-    return WORKOUT_LIST
+def _get_list(db: Session, page):
+    return crud.get_workouts(db, page)
 
 
-def _get_item(item_id):
-    return WORKOUT_LIST[item_id - 1]
+def _get_item(db: Session, _id):
+    return crud.get_workout(db, _id)
 
 
-def _create_item(schema: WorkoutCreateModel):
-    workout = Workout(
-        id=len(WORKOUT_LIST) + 1,
+def _create_item(db: Session, schema: WorkoutCreateModel):
+    new_model = models.Workout(
         name=schema.name,
-        description=schema.description
+        description=schema.description,
     )
-    WORKOUT_LIST.append(workout.dict())
-    return workout
-
+    return crud.create_workout(db, new_model)
